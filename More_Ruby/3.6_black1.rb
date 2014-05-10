a2f2c9977bd4ec604f5e9531953926e7b80d07d7
beuneche <laurent@beuneche.org>
2014-05-10 14:18:11 -0300

    3.6 works

--- More_Ruby/3.6_black1.rb
+++ More_Ruby/3.6_black1.rb
@@ -1,10 +1,4 @@
 ['banana','anana','naan'].map do |food|
   food.reverse
-end.select { |f| puts f.match /^a/ }
-
-
-#['banana','anana','naan'].map do |food|
-#  puts food.reverse
-#
-#end
+end.select { |f| f.match /^a/ }
 
