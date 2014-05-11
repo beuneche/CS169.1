5d8058df71e19dbead7ad9705fc55834e832acb3
beuneche <laurent@beuneche.org>
2014-05-11 12:52:26 -0300

    calisthenics

---
+++ HW1/ruby-calisthenics/lib/fun_with_strings.rb
@@ -0,0 +1,19 @@
+module FunWithStrings
+  def palindrome?
+    s = self.downcase.gsub(/\W/, "").squeeze(" ").strip
+    s == s.reverse
+  end
+  def count_words
+    # your code here
+  end
+  def anagram_groups
+    # your code here
+  end
+end
+
+# make all the above functions available as instance methods on Strings:
+
+class String
+  include FunWithStrings
+  puts "A man, a plan, a canal -- Panama".palindrome?
+end
