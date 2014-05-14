0d55754e6db05bba4ac317ef4bc840d9e9537182
beuneche <laurent@beuneche.org>
2014-05-14 17:44:51 -0300

    RPS half ok

--- HW1/ruby-calisthenics/lib/rock_paper_scissors.rb
+++ HW1/ruby-calisthenics/lib/rock_paper_scissors.rb
@@ -4,7 +4,14 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    if !!(player1[1] =~ /[RSP]/) and !!(player2[1] =~ /[RSP]/)
+    then
+      player1[1] == "R" and player2[1] == "P" ? (return player2) : (return player1)
+      player1[1] == "S" and player2[1] == "R" ? (return player2) : (return player1)
+      player1[1] == "P" and player2[1] == "S" ? (return player2) : (return player1)
+    else
+      raise NoSuchStrategyError, "Strategy must be one of R,P,S"
+    end
   end
 
   def self.tournament_winner(tournament)
@@ -12,3 +19,8 @@ class RockPaperScissors
   end
 
 end
+
+puts RockPaperScissors.winner(['Armando','R'], ['Dave','S'])  # => ['Dave','S']
+
+
+
